0a355cc830475177e61077176fcec811ed6c99ff
Emily Lim <lim.emi@outlook.com>
2022-02-09 22:42:00 -0800

    Set up server
    Started code with socket.io to set up a running server.

---
+++ src/Emily/convo_server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "OPTIONS"],
+    },
+});
+
+io.on("connection", (socket) => { // emit the event "connection" and listen for socket to happen
+    console.log(`User with ID: ${socket.id} connected!`);
+
+    socket.on("disconnect", () => {
+        console.log(`User with ID: ${socket.id} disconnected!`);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("Server running!");
+});
