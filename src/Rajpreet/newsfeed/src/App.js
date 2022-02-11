d522231d8d8d90d8753337bb37e93a47aed87418
Raj Judge <43121310+judgeraj@users.noreply.github.com>
2022-02-11 15:12:16 -0800

    very basic news for one topic
    Single topic news, need to preform css to improve appereance

--- src/Rajpreet/newsfeed/src/App.js
+++ src/Rajpreet/newsfeed/src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
     this.state = {
       businessArticles:[]
     }
+    // this.state = {
+    //   techArticles: []
+    // }
   }
 
   componentDidMount() {
@@ -24,19 +27,36 @@ class App extends Component {
         businessArticles: data.articles
       });
     });
+    // fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=876fb32423264a94984a112b0dfea143')
+    // .then((response) => {
+    //   return response.json()
+    // })
+    // .then((data) => {
+    //   this.setState({
+    //     techArticles: data.articles
+    //   });
+    // });
+
   }
 
   render() {
     console.log(this.state);
+
     return (
      <div className="App">
         {this.state.businessArticles.map((article, iterator)=> {
           return (
-            <div> 
-              <h2 style={{textAlign:'left'}}>
-                {article.title}
-              </h2>
-              <img src={article.urlToImage}></img>
+            <div id='singleArticle'> 
+              <a href={article.url}>
+                <img src={article.urlToImage} className='image'></img>
+              </a>
+              <a href={article.url}>
+                <h2>
+                  {article.title}
+                </h2>
+              </a>
+              <p>{article.description}</p>
+              <a href={article.url}>{article.url}</a>
             </div>
           );
           })}
