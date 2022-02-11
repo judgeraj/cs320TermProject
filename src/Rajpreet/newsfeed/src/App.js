e75fa8968e1802ab12d5f0c6ea830c1ebede4da9
Raj Judge <43121310+judgeraj@users.noreply.github.com>
2022-02-11 10:35:24 -0800

    Minor Changes
    Making API to get business news

--- src/Rajpreet/newsfeed/src/App.js
+++ src/Rajpreet/newsfeed/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+async function getBusinessNews() { 
+  // let response = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=876fb32423264a94984a112b0dfea143');
+  // let data = response.json();
+  //return data;
+  fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=876fb32423264a94984a112b0dfea143')
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+class App extends Component {
+  render() {
+    var data = getBusinessNews();
+    // console.log(data);
+    return (
+     <div className="App">
+        Person name newsfeed
+      </div>
+    );
+  }
 }
 
 export default App;
