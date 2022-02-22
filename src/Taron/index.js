01655158732f850bf5b09579d927014836c909f0
Taron Linge <97817795+taronlinge@users.noreply.github.com>
2022-02-21 19:00:05 -0800

    Need api contract and auth middleware

--- src/Taron/index.js
+++ src/Taron/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv/config');
 
+// Express is the wrapper for all http stuff
 const app = express();
-const serverPort = process.env.PORT || 3000;
+const serverPort = process.env.PORT || 3000;   // If 3000 in use, process will find an available port
 
-app.use(bodyParser.json());
+// Middlewares
+// TODO: add function for authorization on the pages!!!!
+app.use(cors());              // Allow all cors requests
+app.use(bodyParser.json());   // Trust json in request bodies
 
 // Import routes
 const animeRoute = require('./routes/anime');
@@ -14,25 +19,23 @@ const chatsRoute = require('./routes/chats');
 const gamesRoute = require('./routes/games');
 const showsRoute = require('./routes/shows');
 
+// Route middlewares
 app.use('/anime', animeRoute);
 app.use('/chats', chatsRoute);
 app.use('/games', gamesRoute);
 app.use('/shows', showsRoute);
 
-// Middlewares
-// auth??
-
-// Main route
+// Main route, not really a home page
 app.get('/', (req, res) => {
     res.send('GET request to home page');
 });
 
-// Connect to DB
+// Connect to DB, NoSQL since I don't know SQL enough
 mongoose.connect(process.env.MONGO_URL)
         .then(() => console.log('Database connected'))
         .catch(err => console.log(err));
 
-// Listen for connections
+// Good to go, listen for connections
 app.listen(serverPort, () =>
     console.log(`Server running on port ${serverPort}`)
 );
--- src/Taron/routes/shows.js
+++ src/Taron/routes/shows.js
@@ -1,22 +1,84 @@
 const express = require('express');
 const Show = require('../models/Show');
 const router = express.Router();
- 
-router.get('/', (req, res) => {
-    res.send('GET request to Vivian\'s shows page');
+
+// Get all the shows, maybe for a listing of all shows?
+router.get('/', async (req, res) => {
+    // debug the GET request
+    console.log('GET request to Vivian\'s shows page');
+
+    // await getting the shows from the database
+    try {
+        const shows = await Show.find();
+        res.json(shows);
+    } catch(err) {
+        res.json({ message: err });
+    }
 });
 
+// Get a specific show, not sure the use case?
+// Maybe opening a modal window to modify the show's information?
+router.get('/:showId', async (req, res) => {
+    // debug the params
+    console.log('GET request to Vivian\'s shows page');
+    console.log(req.params.showId);
+
+    try {
+        const show = await Show.findById(req.params.showId);
+        res.json(show);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+// Post a single show, maybe from Add Show or something?
 router.post('/', async (req, res) => {
-    //console.log(req.body);
+    // debug the POST contents
+    console.log('POST request to Vivian\'s shows page');
+    console.log(req.body);
+
+    // create a new Show Model from the ShowSchema
     const show = new Show({
         title: req.body.title,
         description: req.body.description,
         rating: req.body.rating
     });
     
+    // try to save the posted data to the database
     try {
         const savedShow = await show.save();
         res.json(savedShow);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+// Update part of a show
+router.patch('/:showId', async (req, res) => {
+    // debug the params
+    console.log('PATCH request to Vivian\'s shows page');
+    console.log(req.params.showId);
+
+    try {
+        const updatedShow = await Show.updateOne(
+            { _id: req.params.showId },
+            { $set: { title: req.body.title } }
+        );
+        res.json(updatedShow);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+// Delete a show by showId
+router.delete('/:showId', async (req, res) => {
+    // debug the params
+    console.log('DELETE request to Vivian\'s shows page');
+    console.log(req.params.showId);
+
+    try {
+        const removedShow = await Show.deleteOne({ _id: req.params.showId });
+        res.json(removedShow);
     } catch(err) {
         res.json({ message: err });
     }
