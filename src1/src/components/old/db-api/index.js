b630f294b65e17c830d0d70dda95647d978e5794
Taron Linge <97817795+taronlinge@users.noreply.github.com>
2022-04-21 14:07:39 -0700

    adding user profile page

--- src1/src/components/old/db-api/index.js
+++ src1/src/components/old/db-api/index.js
@@ -1,40 +1,46 @@
 // index.js
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cors = require('cors');
+/* This was the first pass at implementing a database
+ * for storing data for our app. Between a db, logins,
+ * and an api, we decided to use Google Firebase.
+ * This is old code that isn't used in the final app.
+ */
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // Import routes
-const mainRoute = require('./routes/main');
-const animeRoute = require('./routes/anime');
-const chatsRoute = require('./routes/chats');
-const gamesRoute = require('./routes/games');
-const showsRoute = require('./routes/shows');
+const mainRoute = require("./routes/main");
+const animeRoute = require("./routes/anime");
+const chatsRoute = require("./routes/chats");
+const gamesRoute = require("./routes/games");
+const showsRoute = require("./routes/shows");
 
 // Express is the wrapper for all http stuffz
 const app = express();
-const serverPort = 3000;   // Use 300 if no port set in env vars (process.env.PORT, 2020)
+const serverPort = 3000; // Use 300 if no port set in env vars (process.env.PORT, 2020)
 
 // Get the database connection string from ./.env
 dotenv.config();
 
 // Middlewares
-app.use(cors());           // Allow all cors requests
-app.use(express.json());   // Use the built-in body parser to parse json
+app.use(cors()); // Allow all cors requests
+app.use(express.json()); // Use the built-in body parser to parse json
 
 // Route middlewares
-app.use('/', mainRoute);
-app.use('/anime', animeRoute);
-app.use('/chats', chatsRoute);
-app.use('/games', gamesRoute);
-app.use('/shows', showsRoute);
+app.use("/", mainRoute);
+app.use("/anime", animeRoute);
+app.use("/chats", chatsRoute);
+app.use("/games", gamesRoute);
+app.use("/shows", showsRoute);
 
 // Connect to DB, NoSQL MongoDB since I no SQL
-mongoose.connect(process.env.MONGO_URL)
-        .then(() => console.log('Database connected'))
-        .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
 
 // Good to go, listen for connections
 app.listen(serverPort, () =>
-    console.log(`Server running on port ${serverPort}`)
+  console.log(`Server running on port ${serverPort}`)
 );
---
+++ src1/src/components/profile/UserProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserProfile = () => {
+  return (
+    <div>
+      <p>haha</p>
+    </div>
+  );
+};
+
+export default UserProfile;
