8e85b1d5d661047cf4a140961cf93e65562ceca5
Taron Linge <97817795+taronlinge@users.noreply.github.com>
2022-04-23 18:15:32 -0700

    cleaned up homepage

--- src1/src/components/HomePage.js
+++ src1/src/components/HomePage.js
@@ -1,33 +1,31 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../features/userSlice';
-import database from './../firebase/firebase';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import database from "./../firebase/firebase";
 
 function HomePage() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
   useEffect(() => {
-    addUser()
-  }, [])
+    addUser();
+  }, []);
 
   const addUser = () => {
-    const userName = database.collection('users').doc(user.displayName)
-    userName.get()
-    .then((docSnapshot) => {
-        if (docSnapshot.exists) {
+    const userName = database.collection("users").doc(user.displayName);
+    userName.get().then((docSnapshot) => {
+      if (docSnapshot.exists) {
         userName.onSnapshot((doc) => {
-            // do stuff with the data
+          // do stuff with the data
         });
-        } else {
-            userName.set({
-                photo: user.photo,
-            });
-        }
+      } else {
+        userName.set({
+          photo: user.photo,
+        });
+      }
     });
-  }
+  };
   return (
     <>
       <h3>Homepage... what should go here?</h3>
-      <p>this is a complete disaster... i don't like javascript/html/css</p>
     </>
   );
 }
